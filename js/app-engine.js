9eba22f6a1df3e265ac353012ddfb7e4233422e5
Kris Renaldi <krenaldi@comcast.net>
2016-03-06 21:23:44 -0500

    Redid entire files
    Redid entire files and added oauth-signature script

---
+++ js/app-engine.js
@@ -0,0 +1,199 @@
+// Initialize the map
+var map;
+
+// Initialize the default infoWindow
+var infoWindow = new google.maps.InfoWindow({
+  // default content
+  content: '<div><h4 id="venue-name"></h4><p id="venue-address"></p><p id="yelp"></p></div>'
+});
+
+// Set up the ViewModel
+var ViewModel = function() {
+  'use strict';
+
+  var self = this;
+  self.venueList = ko.observableArray([]);
+  self.filteredVenueList = ko.observableArray([]);
+
+  // Create the google map zoomed in on Manhattan
+  self.initialize = function() {
+    var mapCanvas = document.getElementById('google-map');
+    var cenLatLng = new google.maps.LatLng(40.750547, -73.993508);
+    var mapOptions = {
+      center: cenLatLng,
+      zoom: 10,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    map = new google.maps.Map(mapCanvas, mapOptions);
+  };
+
+  // Create the list of Venue locations from the model
+  self.buildVenueLocations = function() {
+    venueLocations.forEach(function(venueItem) {
+      self.venueList.push( new Venue(venueItem) );
+    });
+  };
+
+  // Set up an event listener for clicks for each venue
+  self.setVenueClickFunctions = function() {
+    self.venueList().forEach(function(venue) {
+      google.maps.event.addListener(venue.marker(), 'click', function() {
+        self.venueClick(venue);
+      });
+    });
+  };
+
+  // Function to handle clicking on a venue (either in list or marker)
+  self.venueClick = function(venue) {
+    // Set the content of the infoWindow
+    var infoContent = '<div><h4 id="venue-name">' + venue.name() + '</h4>' +
+                      '<h5 id="venue-address">' + venue.address() + '</h5>' +
+                      '<h6 id="venue-neighborhood">' + venue.neighborhood() + '</h6>' +
+                      '<p id="text">Rating on <a id="yelp-url">yelp</a>: ' +
+                      '<img id="yelp"></p></div>';
+    infoWindow.setContent(infoContent);
+    self.getYelpData(venue);
+
+    // Make the clicked on venue the center of the map
+    map.panTo(new google.maps.LatLng(venue.lat(), venue.lng()));
+
+    // Open the infoWindow at the marker location
+    infoWindow.open(map, venue.marker());
+
+    // Current venue marker bounces once when clicked
+    self.setMarkerAnimation(venue);
+  };
+
+  // Sets the currenter marker to bounce once when clicked
+  self.setMarkerAnimation = function(venue) {
+    venue.marker().setAnimation(google.maps.Animation.BOUNCE);
+    setTimeout( function() { venue.marker().setAnimation(null); }, 750);
+  };
+
+  // Function to handle filtering of venues based on the search form
+  self.filterVenues = function() {
+    // Set the filtered venue list to an empty array
+    self.filteredVenueList([]);
+
+    // Get the search string and the length of the original venue list
+    var searchString = $('#search-str').val().toLowerCase();
+    var len = self.venueList().length;
+
+    // Loop through each venue in the venue list
+    for (var i = 0; i < len; i++) {
+      // Get the current venue name & neighborhood
+      var venueName = self.venueList()[i].name().toLowerCase();
+      var venueNeighborhood = self.venueList()[i].neighborhood().toLowerCase();
+
+      // If the name or neighborhood match the search string,
+      // add the venue to the filtered venue list
+      if (venueName.indexOf(searchString) > -1 ||
+          venueNeighborhood.indexOf(searchString) > -1) {
+        self.filteredVenueList.push(self.venueList()[i]);
+        // Set the map property of the marker to the map
+        self.venueList()[i].marker().setMap(map);
+      } else {
+        // Set the map property of the marker to null so it won't be visible
+        self.venueList()[i].marker().setMap(null);
+      }
+    }
+  };
+
+  self.getYelpData = function(venue) {
+    // Uses the oauth-signature package installed with bower per https://github.com/bettiolo/oauth-signature-js
+
+    // Use the GET method for the request
+    var httpMethod = 'GET';
+
+    // Yelp API request url
+    var yelpURL = 'http://api.yelp.com/v2/search/';
+
+    // nonce generator
+    // function credit of: https://blog.nraboy.com/2015/03/create-a-random-nonce-string-using-javascript/
+    var nonce = function(length) {
+        var text = "";
+        var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        for(var i = 0; i < length; i++) {
+            text += possible.charAt(Math.floor(Math.random() * possible.length));
+        }
+        return text;
+    };
+
+    // Set required parameters for authentication & search
+    var parameters = {
+      oauth_consumer_key: 'QaViCkyHyj1wY-smmXh6yQ',
+      oauth_token: 'bSMpYC1wYz5KRYRpGGUq6SaIkIcaAMhg',
+      oauth_nonce: nonce(20),
+      oauth_timestamp: Math.floor(Date.now() / 1000),
+      oauth_signature_method: 'HMAC-SHA1',
+      oauth_version: '1.0',
+      callback: 'cb',
+      term: venue.name(),
+      location: venue.location(), 
+      limit: 1
+    };
+
+    // Set other API parameters
+    var consumerSecret = 'SpoFppLZKTB7TLQOjTcu1m9yWDs';
+    var tokenSecret = 'oh2l1S9842JilfjN2y5q1TfFv2E';
+
+    // generates a RFC 3986 encoded, BASE64 encoded HMAC-SHA1 hash
+    var signature = oauthSignature.generate(httpMethod, yelpURL, parameters, consumerSecret, tokenSecret);
+
+    // Add signature to list of parameters
+    parameters.oauth_signature = signature;
+
+    // Set up the ajax settings
+    var ajaxSettings = {
+      url: yelpURL,
+      data: parameters,
+      cache: true,
+      dataType: 'jsonp',
+      success: function(response) {
+        // Update the infoWindow to display the yelp rating image
+        $('#yelp').attr("src", response.businesses[0].rating_img_url);
+        $('#yelp-url').attr("href", response.businesses[0].url);
+      },
+      error: function() {
+        $('#text').html('Data could not be retrieved from yelp.');
+      }
+    };
+
+    // Send off the ajaz request to Yelp
+    $.ajax(ajaxSettings);
+  };
+
+  // Add the listener for loading the page
+  google.maps.event.addDomListener(window, 'load', function() {
+    self.initialize();
+    self.buildVenueLocations();
+    self.setVenueClickFunctions();
+    self.filteredVenueList(self.venueList());
+  });
+};
+
+// Venue constructor to create venues & marks from the model
+var Venue = function(data) {
+  'use strict';
+
+  // Set all the properties as knockout observables
+  var marker;
+  this.name = ko.observable(data.name);
+  this.lat = ko.observable(data.lat);
+  this.lng = ko.observable(data.lng);
+  this.address = ko.observable(data.address);
+  this.neighborhood = ko.observable(data.neighborhood);
+
+  // Google Maps Marker for this location
+  marker = new google.maps.Marker({
+    position: new google.maps.LatLng(this.lat(), this.lng()),
+    map: map,
+    title: this.name()
+  });
+
+  // Set the marker as a knockout observable
+  this.marker = ko.observable(marker);
+};
+
+// Kick everything off!
+ko.applyBindings( new ViewModel() );
---
+++ js/guide.js
@@ -0,0 +1,139 @@
+/* List of venues to be pulled by app-engine.js for listing in Yelp */
+var venueListings = [
+    {
+        date: 'Mar 05',
+        location: 'New York, NY',
+        name: "O'Flanagan's",
+        lat: 40.763865,
+        lng: -73.959315,
+        address: '1215 1st Ave, New York, NY 10065',
+        neighborhood: 'Upper East Side'
+    },
+    {
+        
+        date: 'Mar 12',
+        location: 'Scotch Plains, NJ',
+        name: "Stage House Tavern",
+        lat: 40.650870,
+        lng: -74.398812,
+        address: '366 Park Ave, Scotch Plains, NJ 07076',
+        neighborhood: ''
+    },
+    {
+        date: 'Mar 19',
+        location: 'Staten Island, NY',
+        name: 'Hot Shotz',
+        lat: 40.523310,
+        lng: -74.216272,
+        address: '6321 Amboy Rd, Staten Island, NY 10309',
+        neighborhood: 'Princes Bay'
+    },
+    {
+        date: 'Apr 09',
+        location: 'New York, NY',
+        name: 'Prohibition',
+        lat: 40.785216,
+        lng: -73.972812,
+        address: '503 Columbus Ave, New York, NY 10024',
+        neighborhood: 'Upper West Side'
+    },
+    {
+        date: 'Apr 15',
+        location: 'Hackensack, NJ',
+        name: "Crow's Nest",
+        lat: 40.875223,
+        lng: -74.064208,
+        address: '309 Vincent Ave, Hackensack, NJ 07601',
+        neighborhood: ''
+    },
+    {
+        date: 'Apr 22',
+        location: 'New York, NY',
+        name: "McGettigan's",
+        lat: 40.750625,
+        lng: -73.986298,
+        address: '70 W 36th St, New York, NY 10018',
+        neighborhood: 'Midtown West'
+    },
+    {
+        date: 'May 06',
+        location: 'New Brunswick, NJ',
+        name: 'Court Tavern',
+        lat: 40.495688,
+        lng: -74.445187,
+        address: '124 Church St, New Brunswick, NJ 08901',
+        neighborhood: ''
+    },
+    {
+        date: 'Jun 24',
+        location: 'New York, NY',
+        name: "Smith's Bar",
+        lat: 40.758777,
+        lng: -73.989044,
+        address: '701 8th Ave, New York, NY 10036',
+        neighborhood: 'Theater District'
+    },
+    {
+        date: 'Jul 09',
+        location: 'Point Pleasant Beach, NJ',
+        name: 'Boardwalk Bar & Grill',
+        lat: 40.093593,
+        lng: -74.036957,
+        address: '401 Boardwalk, Point Pleasant Beach, NJ 08742',
+        neighborhood: ''
+    },
+    {
+        date: 'Aug 05',
+        location: 'Lake Como, NJ',
+        name: 'Bar Anticipation',
+        lat: 40.174402,
+        lng: -74.028350,
+        address: '703 16th Ave, Lake Como, NJ 07719',
+        neighborhood: ''
+    },
+    {
+        date: 'Aug 06',
+        location: 'Neptune City, NJ',
+        name: 'The Headliner',
+        lat: 40.188879,
+        lng: -74.025846,
+        address: '1401 State Route 35 S, Neptune City, NJ 07753',
+        neighborhood: ''
+    },
+    {
+        date: 'Sep 23',
+        location: 'Maywood, NJ',
+        name: 'Twin Door Tavern',
+        lat: 40.905556,
+        lng: -74.064807,
+        address: '122 W Pleasant Ave, Maywood NJ 07607',
+        neighborhood: ''
+    },
+    {
+        date: 'Oct 14',
+        location: 'Staten Island, NY',
+        name: "Killmeyer's Old Bavarian Inn",
+        lat: 40.538459,
+        lng: -74.237136,
+        address: '4254 Arthur Kill Rd, Staten Island, NY 10309',
+        neighborhood: ''
+    },
+    {
+        date: 'Nov 11',
+        location: 'Piscataway, NJ',
+        name: "Gabriele's Bar & Grill",
+        lat: 40.553071,
+        lng: -74.435015,
+        address: '1351 Centennial Ave, Piscataway, NJ 08854',
+        neighborhood: ''
+    },
+    {
+        date: 'Dec 10',
+        location: 'Red Bank, NJ',
+        name: 'The Downtown',
+        lat: 40.352261,
+        lng: -74.065804,
+        address: '10 W Front St, Red Bank, NJ 07701',
+        neighborhood: ''
+    },
+];
